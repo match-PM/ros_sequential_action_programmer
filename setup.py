39622fb6cc2970d57693e030fff3f406cad25d25
Florwie <wiemann@match.uni-hannover.de>
2026-02-10 17:17:27 +0100

    co-pilot integration

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ submodules_action_classes = 'ros_sequential_action_programmer/submodules/action_
 submodules_obj_dict_classes = 'ros_sequential_action_programmer/submodules/obj_dict_modules'
 submodules_app = 'ros_sequential_action_programmer/submodules/RsapApp_submodules'
 pm_robot_modules = 'ros_sequential_action_programmer/submodules/pm_robot_modules'
-co_pilot = 'ros_sequential_action_programmer/submodules/co_pilot'
 submodules_rsap= 'ros_sequential_action_programmer/submodules/rsap_modules'
 
 setup(
@@ -14,14 +13,13 @@ setup(
     version='0.0.0',
     #packages=find_packages(exclude=['test']),
     #packages=find_packages(package_name,submodules),   
-    packages=(package_name,submodules, submodules_action_classes, submodules_app, submodules_obj_dict_classes, pm_robot_modules, co_pilot,submodules_rsap),   
+    packages=(package_name,submodules, submodules_action_classes, submodules_app, submodules_obj_dict_classes, pm_robot_modules, submodules_rsap),   
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['config/blacklist.yaml']),
         ('share/' + package_name, ['config/whitelist.yaml']),
-        ('share/' + package_name, ['config/OpenAI_config.yaml']),
         ('share/' + package_name, ['config/path_definitions.yaml']),
         ('share/' + package_name, ['config/launch_files.yaml']),
         ('share/' + package_name, ['config/app_icon.png']),
